fcb1f08258e578b88364e66a1e23dd1a7d66cb31
yet1009 <84160197+yet1009@users.noreply.github.com>
2022-05-24 23:04:22 +0900

    revise

--- attraction/src/index.js
+++ attraction/src/index.js
@@ -1,3 +1,38 @@
 import Store from './store.js';
+import { qs, qsAll, delegate, on } from './dom.js';
+
+function tempHeight() {
+
+    let box1 = qs('.box1');
+    let box3 = qs('.box3');
+    let obj = {
+        height: document.documentElement.clientHeight,
+        num: 4,
+    } 
+    let { height, num } = obj;
+
+    box1.style.height = (height * 4) + "px";
+    box1.style.border = "1px solid red"
+
+    box3.style.height = (height * 4) + "px";
+    box3.style.border = "1px solid pink"
+
+}
+
+
+function FixedSection() {
+
+    this.box2 = qs('.box2');
+
+}
+
+
+
+
+on(document, 'DOMContentLoaded', tempHeight)
+
+
+on(document, 'scroll', () => { FixedSection() })
+
 
 
